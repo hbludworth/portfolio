6f3206fdf2a3bebe663a446ed00a80981d9fa008
Harrison Bludworth <harrisonbludworth@gmail.com>
2023-05-19 16:50:03 -0600

    basic structure

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <RouterLink class="navbar-item" to="/"> Home </RouterLink>
+
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarMenu"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarMenu" class="navbar-menu">
+        <div class="navbar-end">
+          <RouterLink class="navbar-item" to="/projects"> Projects </RouterLink>
+
+          <RouterLink class="navbar-item" to="/writing"> Writing </RouterLink>
+
+          <RouterLink class="navbar-item" to="/resume"> Resume </RouterLink>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,42 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main><span>CHEESE</span></main>
+  <section class="hero has-text-centered">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <h1 class="title is-spaced is-size-1-desktop is-size-2-tablet is-size-3-mobile">
+              Hi, I'm Harrison
+            </h1>
+            <h2 class="subtitle is-size-4-tablet">I design and build quality software products.</h2>
+            <img id="avatar" src="@/assets/profile.jpeg" class="mt-4" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="hero-foot">
+      <div class="container"></div>
+    </div>
+  </section>
+  <section class="section is-medium has-text-centered has-background-primary">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-three-fifths">
+          <h1 class="title is-spaced is-size-3-desktop is-size-4-mobile has-text-white">
+            Full Stack Web Developer, Project Manager, and Backend Engineer
+          </h1>
+          <h2 class="subtitle is-size-5-desktop has-text-weight-normal has-text-white">
+            I'm a full stack web developer with a passion for building beautiful, functional, and
+            scalable web applications. I've developed many projects in various positions including
+            as a full-time student, a freelance developer, a project manager, and even as a
+            technical cofounder. I excel at understanding complex problems, building powerful
+            solutions, and developing products that customers need. Having recently graduated in
+            Entrepreneurship and Computer Science, I am currently looking for a full-time position
+            as a software engineer and am confident that I can be a valuable asset to any team.
+          </h2>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
