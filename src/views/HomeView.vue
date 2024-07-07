684e324423479ae52e21abf79f4077adbb57c7bb
Harrison Bludworth <harrisonbludworth@gmail.com>
2024-07-06 19:51:44 -0600

    updated information

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -35,18 +35,17 @@
       <div class="columns is-centered">
         <div class="column is-three-fifths">
           <h1 class="title is-spaced is-size-3-desktop is-size-4-mobile has-text-white title-font">
-            Full Stack Developer, Project Manager, and Technical Cofounder
+            Experienced Full Stack Software Engineer
           </h1>
           <h2
             class="subtitle is-size-5-desktop has-text-weight-normal has-text-white description-box"
           >
-            I'm a full stack web developer with a passion for building beautiful, functional, and
-            scalable web applications. I've developed many projects in various positions including
-            as a full-time student, a freelance developer, a project manager, and even as a
-            technical cofounder. I excel at understanding complex problems, building powerful
-            solutions, and developing products that customers need. Having recently graduated in
-            Entrepreneurship and Computer Science, I am currently looking for a full-time position
-            as a software engineer and am confident that I can be a valuable asset to any team.
+            I'm an experienced full stack software engineer with a passion for building beautiful,
+            functional, and scalable web applications. I currently work as a full stack engineer at
+            Fidelity Investments. I've developed many projects in various positions including as a
+            full-time employee, a freelance developer, a project manager, and even as a technical
+            cofounder. I excel at understanding complex problems, building powerful solutions, and
+            developing products that customers need.
           </h2>
         </div>
       </div>
@@ -83,9 +82,8 @@
           <h2
             class="subtitle is-size-5-desktop has-text-weight-normal has-text-white description-box"
           >
-            During my three years as a full stack developer, and as an Entrepreneurship and Computer
-            Science student at Brigham Young University, I've had the opportunity to work on many
-            fun, challenging, and rewarding projects. I've used many different technologies and
+            During my time as a full stack software engineer, I've had the opportunity to work on
+            many fun, challenging, and rewarding projects. I've used many different technologies and
             languages to build some cool stuff. Here are a few of my favorites.
           </h2>
         </div>
@@ -203,9 +201,8 @@
               ></a>
             </span>
           </p>
-          <p class="mt-6">Made by me. © 2023.</p>
+          <p class="mt-6">Made by me. © 2024.</p>
           <p class="mt-5 is-size-7">Built with Vue, Bulma, and TypeScript.</p>
-          <p class="mt-1 is-size-7">Deployed on AWS S3.</p>
           <a
             href="https://github.com/hbludworth/portfolio"
             target="_blank"
@@ -238,7 +235,7 @@ interface SkillColumnTemplate {
 const skillColumnData: Ref<SkillColumnTemplate[]> = ref([
   {
     icon: 'mdi-xml',
-    title: 'Full Stack Developer',
+    title: 'Full Stack Software Engineer',
     description:
       'I can build beautiful, functional, and scalable web applications from the ground up.',
     subtitle1: 'Dev Tools',
@@ -329,7 +326,7 @@ const myProjects: Ref<ProjectTemplate[]> = ref([
     title: 'OARE',
     imagePath: 'oare.png',
     description:
-      'The Old Assyrian Research Environment is a digital humanities project at BYU that helps historians analyze and perform research on ancient cuneiform tablets. It is built with Vue, Node.js, TypeScript, and SQL. I am the primary contributor to the project.',
+      'The Old Assyrian Research Environment is a digital humanities project at BYU that helps historians analyze and perform research on ancient cuneiform tablets. It is built with Vue, Node.js, TypeScript, and SQL. During my time at BYU, I was the primary contributor to the project.',
     viewLink: 'https://oare.byu.edu',
     githubLink: 'https://github.com/oaregithub/oare_mono',
   },
@@ -337,8 +334,7 @@ const myProjects: Ref<ProjectTemplate[]> = ref([
     title: 'MicDrop',
     imagePath: 'micdrop.png',
     description:
-      'MicDrop is a Chrome extension that allows users to send and receive audio messages from within Gmail. It allows for basic customization and is built with Vue, Node.js, and TypeScript. (Note: Landing page is not my work. The full product itself is.)',
-    viewLink: 'https://www.sendmicdrop.com',
+      'MicDrop was a Chrome extension that allowed users to send and receive audio messages from within Gmail. It allowed for basic customization and was built with Vue, Node.js, and TypeScript. I was the technical cofounder and primary developer before MicDrop was shut down.',
     githubLink: 'https://github.com/hbludworth/micdrop',
   },
   {
@@ -384,9 +380,14 @@ interface ExperienceTemplate {
 
 const myExperience: Ref<ExperienceTemplate[]> = ref([
   {
+    company: 'Fidelity Investments',
+    position: 'Full Stack Software Engineer',
+    time: 'August 2023 - Present',
+  },
+  {
     company: 'Brigham Young University - Department of History',
     position: 'Full Stack Developer - Team Lead - Project Manager',
-    time: 'October 2020 - Present',
+    time: 'October 2020 - July 2023',
   },
   {
     company: 'MicDrop',
