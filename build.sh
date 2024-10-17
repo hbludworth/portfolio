d41eb542b29a7d6696a1fc7a43c90df7cc630217
Harrison Bludworth <harrisonbludworth@gmail.com>
2024-10-16 19:08:13 -0600

    remove express server

--- build.sh
+++
@@ -1,4 +0,0 @@
-run-p type-check build-only
-cd server
-rm -rf dist
-cp -r ../dist .
--- server/app.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.use(express.static('dist'));
-
-app.listen(8081, () => console.log(`Server listening on port 8081`));
